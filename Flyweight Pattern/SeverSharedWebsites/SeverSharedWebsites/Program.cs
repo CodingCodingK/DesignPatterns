069902d195bb7bb62571bd77b57a1b34035cf321
YAN JUNJIE <48432483+CodingCodingK@users.noreply.github.com>
2021-06-04 13:37:04 +0800

    20210604

---
+++ Flyweight Pattern/SeverSharedWebsites/SeverSharedWebsites/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeverSharedWebsites
+{
+	// 希望多个网页能使用同一个数据库
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Chinese Output
+			Console.OutputEncoding = Encoding.GetEncoding(936);
+			#endregion
+
+			WebsiteFactory severs = new WebsiteFactory();
+
+			Website a1 = severs.GetWebsiteCategory("电商平台");
+			a1.Use(new User("马"));
+			Website a2 = severs.GetWebsiteCategory("电商平台");
+			a1.Use(new User("刘"));
+
+			Website b1 = severs.GetWebsiteCategory("游戏平台");
+			b1.Use(new User("马"));
+			Website b2 = severs.GetWebsiteCategory("游戏平台");
+			b2.Use(new User("G"));
+
+			Website c1 = severs.GetWebsiteCategory("软件平台");
+			c1.Use(new User("方"));
+
+			severs.ShowWebsiteCount();
+
+			#region Test Wait
+			Console.Read();
+			#endregion
+		}
+	}
+
+	public class WebsiteFactory
+	{
+		/// <summary>
+		/// 服务器列表
+		/// </summary>
+		private Hashtable flyweights = new Hashtable();
+
+		public Website GetWebsiteCategory(string key)
+		{
+			if (!flyweights.ContainsKey((key)))
+				flyweights.Add(key,new ConcreteWebsite(key));
+			return (Website)flyweights[key];
+		}
+
+		public void ShowWebsiteCount()
+		{
+			Console.WriteLine("共享的网站服务器总服务端数为：{0}", flyweights.Count);
+		}
+	}
+
+	public class ConcreteWebsite:Website
+	{
+		private string name;
+
+		public ConcreteWebsite(string n)
+		{
+			name = n;
+		}
+
+		public override void Use(User user)
+		{
+			Console.WriteLine("网站分类：" + name + ",客户：" + user.name);
+		}
+	}
+
+	public abstract class Website
+	{
+		public abstract void Use(User user);
+	}
+
+	public class User
+	{
+		public string name;
+
+		public User(string n)
+		{
+			name = n;
+		}
+	}
+
+
+}
---
+++ Mediator Pattern/CountryMediator/CountryMediator/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryMediator
+{
+	// 有些类似于订阅者-观察者模式
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Chinese Output
+			Console.OutputEncoding = Encoding.GetEncoding(936);
+			#endregion
+
+			var manager = new UnitedNationsSecurityCouncil();
+			var usa = new USA(manager);
+			var japan = new Japan(manager);
+			var india = new India(manager);
+			manager.SetColleague(usa,japan,india);
+			usa.Notify("美国疫情需要支援");
+			japan.Notify("日本疫情需要支援");
+			india.Notify("印度疫情需要支援");
+
+			#region Test Wait
+			Console.Read();
+			#endregion
+		}
+	}
+
+	public class UnitedNationsSecurityCouncil : UnitedNations
+	{
+		private USA usa;
+		private Japan japan;
+		private India india;
+
+		public void SetColleague(USA c1, Japan c2, India c3)
+		{
+			usa = c1;
+			japan = c2;
+			india = c3;
+		}
+
+		public override void Notify(string message, Country country)
+		{
+			if (country == usa)
+			{
+				japan.GetMessage(message);
+				india.GetMessage(message);
+			}
+			else if (country == japan)
+			{
+				india.GetMessage(message);
+				usa.GetMessage(message);
+			}
+			else if (country == india)
+			{
+				japan.GetMessage(message);
+				usa.GetMessage(message);
+			}
+		}
+	}
+
+	/// <summary>
+	/// 联合国-中介者
+	/// </summary>
+	public abstract class UnitedNations
+	{
+		public abstract void Notify(string message, Country country);
+	}
+
+	public class Country
+	{
+		protected UnitedNations mediator;
+
+		public Country(UnitedNations m)
+		{
+			mediator = m;
+		}
+	}
+
+	public class USA : Country
+	{
+		public USA(UnitedNations m) : base(m) { }
+
+		public void Notify(string message)
+		{
+			mediator.Notify(message,this);
+		}
+
+		public void GetMessage(string message)
+		{
+			Console.WriteLine("美国获得信息：{0}",message);
+		}
+	}
+
+	public class Japan : Country
+	{
+		public Japan(UnitedNations m) : base(m) { }
+
+		public void Notify(string message)
+		{
+			mediator.Notify(message, this);
+		}
+
+		public void GetMessage(string message)
+		{
+			Console.WriteLine("日本获得信息：{0}", message);
+		}
+	}
+
+	public class India : Country
+	{
+		public India(UnitedNations m) : base(m) { }
+
+		public void Notify(string message)
+		{
+			mediator.Notify(message, this);
+		}
+
+		public void GetMessage(string message)
+		{
+			Console.WriteLine("印度获得信息：{0}", message);
+		}
+	}
+
+}
